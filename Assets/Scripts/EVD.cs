b841a6be3b5714d5e56fcb9c2682f9b0f6fbd2e1
Harry Anderson <andeharr@shernet.sheridancollege.ca>
2025-03-13 11:50:42 -0400

    tried to do the first coding challenge

---
+++ Assets/Scripts/EVD.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVD : MonoBehaviour
+{
+    public SpriteRenderer spriteryes;
+    public SpriteRenderer spriterno;
+    public List<SpriteRenderer>onoroffsprites;
+    public void HoveringOver()
+    {
+
+    }
+
+    public void HoveringOverExit()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
